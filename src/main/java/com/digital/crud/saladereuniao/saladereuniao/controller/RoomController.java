1bacc095cd6d4520c99fb0cee6637a329549d7e4
Flavio Monteiro <flaviohnm@gmail.com>
2021-07-19 19:51:19 -0300

    criação dos endpoints

---
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/controller/RoomController.java
@@ -0,0 +1,56 @@
+package com.digital.crud.saladereuniao.saladereuniao.controller;
+
+import com.digital.crud.saladereuniao.saladereuniao.exception.ResourceNotFoundException;
+import com.digital.crud.saladereuniao.saladereuniao.model.Room;
+import com.digital.crud.saladereuniao.saladereuniao.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1")
+public class RoomController {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @GetMapping("/rooms")
+    public List<Room> getAllRooms(){
+        return roomRepository.findAll();
+    }
+
+    @GetMapping("/rooms/id")
+    public ResponseEntity<Room> getRoomById(@PathVariable(value = "id") Long roomId)
+        throws ResourceNotFoundException{
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new ResourceNotFoundException("Room not found:: " + roomId));
+        return ResponseEntity.ok().body(room);
+    }
+
+    @PostMapping("/rooms")
+    public Room createRoom (@Valid @RequestBody Room room){
+        return roomRepository.save(room);
+    }
+
+    @PutMapping("/rooms/{id}")
+    public ResponseEntity<Room> updateRoom(@PathVariable (value = "id") Long roomId,
+                                           @Valid @RequestBody Room roomDetalis ) throws ResourceNotFoundException{
+
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(()-> new ResourceNotFoundException("Room not found this id:: " + roomId));
+        room.setName(roomDetalis.getName());
+        room.setDate(roomDetalis.getDate());
+        room.setStartHour(roomDetalis.getStartHour());
+        room.setEndHour(roomDetalis.getEndHour());
+
+        final Room updateRoom = roomRepository.save(room);
+        return ResponseEntity.ok(updateRoom);
+
+    }
+
+}
