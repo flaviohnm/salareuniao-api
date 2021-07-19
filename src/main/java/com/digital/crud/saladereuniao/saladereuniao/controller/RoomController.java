d6192a551d0fedb9e5fc40b00e220f6559fbc733
Flavio Monteiro <flaviohnm@gmail.com>
2021-07-19 20:38:54 -0300

    criação dos endpoints

--- src/main/java/com/digital/crud/saladereuniao/saladereuniao/controller/RoomController.java
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/controller/RoomController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1")
@@ -53,4 +55,14 @@ public class RoomController {
 
     }
 
+    public Map<String, Boolean> deleteRoom(@PathVariable(value = "id") Long roomId)
+    throws ResourceNotFoundException{
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(()-> new ResourceNotFoundException("Room not found for this id:: " + roomId));
+
+        roomRepository.delete(room);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
 }
